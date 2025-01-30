8f6522bc2a5dc2eb3b88fcb765edd33a5cdf33c6
Simangaliso <74491200+Simangaliso-Hlongwa@users.noreply.github.com>
2025-01-30 06:42:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,20 @@ if menu == "Researcher Profile":
     name = "Simangaliso Hlongwa"
     field = "Computational Chemistry"
     institution = "North-West University"
+    bio = "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metu"
 
     # Display basic profile information
-    st.write(f"**Name:** {name}")
-    st.write(f"**Field of Research:** {field}")
-    st.write(f"**Institution:** {institution}")
+    col1, col2 = column(2)
+    with col1:
+        st.write(f"**Name:** {name}")
+        st.write(f"**Field of Research:** {field}")
+        st.write(f"**Institution:** {institution}")
+        st title("Anout")
+        st.write(f"{bio}")
+
+    with col2:
+        st.image("500x500.jpeg")
+    
 
 elif menu == "Publications":
     st.title("Publications")
